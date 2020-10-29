2c18c989e0c7fdaa68f33a521c4c13b84327b6da
emmy-chen <echen20@stuy.edu>
2020-10-29 10:03:34 -0400

    added constructor

---
+++ BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int accountID, String password) {
+    accountID = accountID;
+    password = password;
+    balance = balance;
+  }
+
+
+}
